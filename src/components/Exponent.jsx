4256e67ed4d5f94b675e720a141ddad39b205626
Miquel Piña Grau <miquel.mpg@gmail.com>
2025-12-12 15:33:57 +0100

    done

--- src/components/Exponent.jsx
+++ src/components/Exponent.jsx
@@ -1,8 +1,8 @@
-const ExponentTwo = ({ count, exponent }) => (
+const Exponent = ({ count, exponent }) => (
    <div className="exponent-counter-container">
     <p className="exponent-label">{count}<sup>{exponent}</sup></p>
     <p className="exponent-result">{`${count}`} {` * ${count}`.repeat(+`${exponent}` - 1)} = <span className="total">{count**exponent}</span></p>
   </div>
 );
 
-export default ExponentTwo;
+export default Exponent;
